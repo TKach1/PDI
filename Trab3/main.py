94e7e8bf025254b5aad5f37f26a5a5987f5e13f3
TKach1 <wesleybr.com@gmail.com>
2024-11-07 19:19:59 -0300

    Trab3 fixes

--- Trab3/main.py
+++ Trab3/main.py
@@ -5,22 +5,27 @@ import cv2
 
 threshold = 0.87
 tamanhoBlur = [15, 15]
-sigma = 2
-borradas = 2
+sigma = 10
+borradas = 3
 
 img = cv2.imread("image.jpg")
 img = img.astype (np.float32) / 255
 
 brightPass = np.where(img < threshold, 0, img)
 
-borrar = cv2.GaussianBlur(brightPass, tamanhoBlur, 3)
-for i in range(borradas):
+borrarGauss = cv2.GaussianBlur(brightPass, [0,0], sigma)
+
+borrar = cv2.blur(brightPass, tamanhoBlur)
+for i in range(borradas-1):
     borrar += cv2.blur(borrar, tamanhoBlur)
 
 cv2.imshow("Bright Pass", brightPass)
 cv2.imshow("Borrado", borrar)
 
 cv2.imshow("Imagem Original", img)
-img = img + borrar
-cv2.imshow("Imagem Final", img)
+imgBox = img + borrar
+cv2.imshow("Imagem Final (BoxBlur)", imgBox)
+imgGauss = img + borrarGauss
+cv2.imshow("Imagem Final (GaussianBlur)", imgBox)
+
 cv2.waitKey(0)
